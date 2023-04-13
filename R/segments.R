107eb86f19a14bd5ac9e3e412d7a96d142dfe376
Juan P. Fonseca-Zamora <69847296+juanfonsecaLS1@users.noreply.github.com>
2023-04-13 14:07:07 +0100

    Update segments.R

--- R/segments.R
+++ R/segments.R
@@ -1,9 +1,9 @@
-library(httr)
-
-headers = c(
-  'X-Api-Key' = 'Your personal API Token comes here.'
-)
-
-res <- VERB("GET", url = "https://telraam-api.net/v1/cameras", add_headers(headers))
-
-cat(content(res, 'text'))
+# library(httr)
+#
+# headers = c(
+#   'X-Api-Key' = 'Your personal API Token comes here.'
+# )
+#
+# res <- VERB("GET", url = "https://telraam-api.net/v1/cameras", add_headers(headers))
+#
+# cat(content(res, 'text'))
