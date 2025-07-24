f2e6e24fb29e960f5ae1e31471479d497b9b3c54
Juan P. Fonseca-Zamora <69847296+juanfonsecaLS1@users.noreply.github.com>
2025-07-24 15:32:37 +0100

    functionalised checking the times to be used in traffic plus

--- R/reading_data.R
+++ R/reading_data.R
@@ -58,36 +58,10 @@ read_telraam_traffic <- function(id,
   report <- match.arg(report)
   tz <- match.arg(tz, choices = OlsonNames())
 
+  checked_times <- check_time_args(time_start,time_end,tz)
 
-  time_start <- ymd_hms(time_start, tz = tz)
-  time_end <- ymd_hms(time_end, tz = tz)
-
-  # Dates check
-  if (is.na(time_start)) {
-    stop("Stard date is not in the correct format i.e. YYYY-MM-DD H:M:S")
-  }
-
-  if (is.na(time_end)) {
-    stop("Stard date is not in the correct format i.e. YYYY-MM-DD H:M:S")
-  }
-
-  if (time_end > Sys.time()) {
-    warning("End time is in the future. End time constained to current system time")
-    time_end <- Sys.time()
-  }
-
-  ## Time conversion into UTC
-  if (tz != "UTC") {
-    tz(time_start) <- "UTC"
-    tz(time_end) <- "UTC"
-  }
-
-
-  # Time difference check
-  if (difftime(time_end, time_start, units = "days") > 90) {
-    warning("Interval is longer than 3 months, end date was set to 90 days after the start date")
-    time_end <- time_start + as.difftime(90, "days")
-  }
+  time_start <- checked_times[[1]]
+  time_end <- checked_times[[2]]
 
 
   # Call preparation
@@ -164,3 +138,17 @@ read_telraam_traffic <- function(id,
 
   return(mydata)
 }
+
+
+read_telraam_traffic_plus <- function(id,
+                                 report = c("per-quarter","per-hour"),
+                                 time_start,
+                                 time_end,
+                                 tz = Sys.timezone(),
+                                 mytoken = get_telraam_token(),
+                                 include_speed = FALSE) {
+
+
+
+}
+
---
+++ R/utils.R
@@ -0,0 +1,36 @@
+check_time_args <- function(time_start,
+                            time_end,
+                            tz
+){
+
+  time_start <- ymd_hms(time_start, tz = tz)
+  time_end <- ymd_hms(time_end, tz = tz)
+
+  # Dates check
+  if (is.na(time_start)) {
+    stop("Stard date is not in the correct format i.e. YYYY-MM-DD H:M:S")
+  }
+
+  if (is.na(time_end)) {
+    stop("Stard date is not in the correct format i.e. YYYY-MM-DD H:M:S")
+  }
+
+  if (time_end > Sys.time()) {
+    warning("End time is in the future. End time constained to current system time")
+    time_end <- Sys.time()
+  }
+
+  ## Time conversion into UTC
+  if (tz != "UTC") {
+    tz(time_start) <- "UTC"
+    tz(time_end) <- "UTC"
+  }
+
+
+  # Time difference check
+  if (difftime(time_end, time_start, units = "days") > 90) {
+    warning("Interval is longer than 3 months, end date was set to 90 days after the start date")
+    time_end <- time_start + as.difftime(90, "days")
+  }
+  return(list(time_start,time_end))
+}
